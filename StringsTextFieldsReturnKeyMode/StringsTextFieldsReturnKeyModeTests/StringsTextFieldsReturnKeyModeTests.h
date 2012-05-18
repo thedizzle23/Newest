e570d4094f58bd713cb2496820675ef819f94469
Robert DeNicola <thedizzle23@gmail.com>
2012-05-18 16:47:28 -0400

    added files

---
+++ StringsTextFieldsReturnKeyMode/StringsTextFieldsReturnKeyModeTests/StringsTextFieldsReturnKeyModeTests.h
@@ -0,0 +1,13 @@
+//
+//  StringsTextFieldsReturnKeyModeTests.h
+//  StringsTextFieldsReturnKeyModeTests
+//
+//  Created by Robert DeNicola on 5/18/12.
+//  Copyright (c) 2012 Developmental Applications. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface StringsTextFieldsReturnKeyModeTests : SenTestCase
+
+@end
---
+++ StringsTextFieldsReturnKeyMode/StringsTextFieldsReturnKeyModeTests/StringsTextFieldsReturnKeyModeTests.m
@@ -0,0 +1,32 @@
+//
+//  StringsTextFieldsReturnKeyModeTests.m
+//  StringsTextFieldsReturnKeyModeTests
+//
+//  Created by Robert DeNicola on 5/18/12.
+//  Copyright (c) 2012 Developmental Applications. All rights reserved.
+//
+
+#import "StringsTextFieldsReturnKeyModeTests.h"
+
+@implementation StringsTextFieldsReturnKeyModeTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in StringsTextFieldsReturnKeyModeTests");
+}
+
+@end
